243943b18fc02424c641eb330550bb4bacb89b68
Léa Raya Décornod <decornod@unistra.fr>
2012-06-08 14:50:16 +0000

    Changed a-propos layouts to a new channel-type called AboutIFrame
    git-svn-id: https://src.unistra.fr/svn/ent2/branches/esup-3.2.4/portail@602 7121ddc7-e143-df11-8979-0016367c88ba

--- custom/uPortal/uportal-impl/src/main/java/fr/unistra/di/metier/dip/ent/portal/portlets/aboutiframe/AboutIFramePortletController.java
+++ custom/uPortal/uportal-impl/src/main/java/fr/unistra/di/metier/dip/ent/portal/portlets/aboutiframe/AboutIFramePortletController.java
@@ -1,23 +1,19 @@
 /**
- * Licensed to Jasig under one or more contributor license
- * agreements. See the NOTICE file distributed with this work
- * for additional information regarding copyright ownership.
- * Jasig licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a
- * copy of the License at:
+ * Copyright (C) 2012 Université de Strasbourg (di-dip@unistra.fr)
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-package org.jasig.portal.portlets.iframe;
+package fr.unistra.di.metier.dip.ent.portal.portlets.aboutiframe;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,36 +21,34 @@ import java.util.Map;
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.portlet.WindowState;
 
 import org.springframework.web.portlet.ModelAndView;
 import org.springframework.web.portlet.mvc.AbstractController;
 
 /**
  * This portlet renders content identified by a URL within an inline browser
- * frame. See
- * <a href="http://www.htmlhelp.com/reference/html40/special/iframe.html">
- * http://www.htmlhelp.com/reference/html40/special/iframe.html</a> for more
- * information on inline frames.
- *
- * @author Susan Bramhall
- * @author Jen Bourey, jbourey@unicon.net
- * @version $Revision$
+ * frame but only if maximized, an about message otherwise.
+ * 
+ * @see org.jasig.portal.AboutIFramePortletController.iframe.IFramePortletController
+ * @author Léa Raya Décornod <decornod@unistra.fr>
  */
-public class IFramePortletController extends AbstractController {
-
-	@Override
-	protected ModelAndView handleRenderRequestInternal(RenderRequest request,
-			RenderResponse response) throws Exception {
-		
-		Map<String,Object> model = new HashMap<String,Object>();
-		
-		// get the IFrame target URL and the configured height of the IFrame
-		// window from the portlet preferences
-		PortletPreferences preferences = request.getPreferences();
-		model.put("url", preferences.getValue("url", null));
-		model.put("height", preferences.getValue("height", null));
-		
-		return new ModelAndView("/jsp/IFrame/iframePortlet", model);
-	}
-
+public class AboutIFramePortletController extends AbstractController {
+    
+    @Override
+    protected ModelAndView handleRenderRequestInternal(RenderRequest request, RenderResponse response) throws Exception {
+        
+        Map<String, Object> model = new HashMap<String, Object>();
+        
+        // get the IFrame target URL and the configured height of the IFrame
+        // window from the portlet preferences
+        PortletPreferences preferences = request.getPreferences();
+        model.put("url", preferences.getValue("url", null));
+        model.put("height", preferences.getValue("height", null));
+        model.put("about", preferences.getValue("about", null));
+        model.put("isAbout", WindowState.NORMAL.equals(request.getWindowState()));
+        
+        return new ModelAndView("/jsp/IFrame/aboutIframePortlet", model);
+    }
+    
 }
--- custom/uPortal/uportal-war/src/main/webapp/WEB-INF/jsp/IFrame/aboutIframePortlet.jsp
+++ custom/uPortal/uportal-war/src/main/webapp/WEB-INF/jsp/IFrame/aboutIframePortlet.jsp
@@ -1,27 +1,34 @@
 <%--
+    Copyright (C) 2012 Université de Strasbourg (di-dip@unistra.fr)
 
-    Licensed to Jasig under one or more contributor license
-    agreements. See the NOTICE file distributed with this work
-    for additional information regarding copyright ownership.
-    Jasig licenses this file to you under the Apache License,
-    Version 2.0 (the "License"); you may not use this file
-    except in compliance with the License. You may obtain a
-    copy of the License at:
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+            http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing,
-    software distributed under the License is distributed on
-    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-    KIND, either express or implied. See the License for the
-    specific language governing permissions and limitations
-    under the License.
-
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 --%>
 
 <%@ include file="/WEB-INF/jsp/include.jsp"%>
 <c:set var="n"><portlet:namespace/></c:set>
-<iframe src="${url}" height="${height}" frameborder="0" width="100%">
-    This browser does not support inline frames.<br/> 
-    <a href="${url}" target="_blank">Click here to view content</a> in a separate window.
-</iframe>
+<c:choose>
+  <c:when test="${isAbout}">
+    <div class="fl-widget portlet" role="section">
+      <div class="fl-widget-content portlet-section-body" role="region">
+        <div class="portlet-section-note" role="note">${about}</div>
+      </div>
+      <div class="fl-widget-content portlet-section-footer" role="region"><a href="<portlet:renderURL windowState="maximized" portletMode="view" />"/>acc&eacute;der au contenu</a></div>
+    </div>
+  </c:when>
+  <c:otherwise>
+    <iframe src="${url}" height="${height}" frameborder="0" width="100%">
+        This browser does not support inline frames.<br/> 
+        <a href="${url}" target="_blank">Click here to view content</a> in a separate window.
+    </iframe>
+  </c:otherwise>
+</c:choose>
